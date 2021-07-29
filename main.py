667de94a1ac55c19b95630f76119a3d2ffeacb43
jackecivre <jackecivre@gmail.com>
2021-07-29 15:56:18 +0300

    last transaction time added

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ def req_wallet(wallet):
     result = requests.get(url)
     # print(result)
 
+
     def data_gathering(link):
         soup = bs4.BeautifulSoup(link.text, "lxml")
         btc = soup.find_all("div", class_="sc-8sty72-0 bFeqhe")
         usd = soup.find_all("div", class_="sc-10m3woc-0 sc-19bnflk-0 GYUxR izJzLI")
+        last_transaction = soup.find_all("div", class_="kad8ah-0 fjudWa")
+
 
         data = []
         keys = []
         values = []
         usd_string = []
+        last_transaction_string = []
+
 
         for i in btc:
             span = i.find("span")
@@ -31,6 +36,11 @@ def req_wallet(wallet):
         for i in usd:
             span = i.find("span")
             usd_string.append(span.string)
+
+        for i in last_transaction:
+            span = i.find("span")
+            last_transaction_string.append(span.string)
+
 
         try:
             usd_print = usd_string[0].split()
@@ -47,15 +57,17 @@ def req_wallet(wallet):
             keys.append(data[6] + " USD")
             keys.append(data[8] + " USD")
             keys.append(data[10] + " USD")
+            keys.append("Last Transaction Date")
             keys.append("Check Date")
-            values.append([data[1]])
-            values.append([data[5]])
-            values.append([data[7]])
-            values.append([data[9]])
-            values.append([data[11]])
-            values.append([usd_values[0].split("(")[1].split(")")[0]])
-            values.append([usd_values[1].split("(")[1].split(")")[0]])
-            values.append([usd_values[2].split("(")[1].split(")")[0]])
+            values.append(data[1])
+            values.append(data[5])
+            values.append(data[7])
+            values.append(data[9])
+            values.append(data[11])
+            values.append(usd_values[0].split("(")[1].split(")")[0])
+            values.append(usd_values[1].split("(")[1].split(")")[0])
+            values.append(usd_values[2].split("(")[1].split(")")[0])
+            values.append(last_transaction_string[0])
             values.append([now_time])
             line = dict(zip(keys, values))
             return line
@@ -85,8 +97,13 @@ def req_wallet(wallet):
 
         try:
             write_to_excel(data)
+
         except Exception as Error:
             print("Writing to Excel Failed " + str(Error))
+
+
+
+
     else:
         try:
             time.sleep(5)
@@ -98,6 +115,7 @@ def req_wallet(wallet):
             write_to_excel(data)
         except Exception as Error:
             print("Writing to Excel Failed " + str(Error))
+
 
 
 if __name__ == '__main__':
